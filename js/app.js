35c78e2aa922075866ef6b49aacd6f97c4832037
artenlf <felipe.arten@gmail.com>
2022-07-21 07:00:24 -0300

    corrigindo máscara monetária

--- js/app.js
+++ js/app.js
@@ -3,17 +3,16 @@ import { valida } from "./validacao.js";
 const inputs = document.querySelectorAll('input');
 
 inputs.forEach(input => {
-
     if (input.dataset.tipo === 'preco') {
-        SimpleMaskMoney.setmask(input, {
+        SimpleMaskMoney.setMask(input, {
             prefix: 'R$ ',
             fixed: true,
             fractionDigits: 2,
             decimalSeparator: ',',
             thousandsSeparator: '.',
             cursor: 'end'
-        });
-    };
+        })
+    }
 
     input.addEventListener('blur', (evento) => {
         valida(evento.target);
